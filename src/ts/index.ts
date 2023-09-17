9f99fb47890cc19f910dd9b4d386a2e60ea87fe4
iojuedioe9rd <catsstudio81@gmail.com>
2023-09-17 15:31:16 +0100

    git 2.0

---
+++ src/ts/index.ts
@@ -0,0 +1,35 @@
+import '../css/global.css';
+import '../scss/global.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.querySelector('main');
+  const counter = main?.querySelector('#counter');
+  const year = document.querySelector('#year');
+
+  if (main && counter) {
+    main.addEventListener('click', (event: MouseEvent) => {
+      const { id, tagName } = event.target as HTMLElement;
+
+      if (tagName === 'BUTTON') {
+        const currentCount = Number(counter.textContent);
+
+        switch (id) {
+          case 'increment': {
+            counter.textContent = `${currentCount + 1}`;
+
+            break;
+          }
+          case 'decrement': {
+            counter.textContent = `${currentCount - 1}`;
+
+            break;
+          }
+        }
+      }
+    });
+  }
+
+  if (year) {
+    year.textContent = new Date().getFullYear().toString();
+  }
+});
